9f77cfaad7e524e80e99ed28479e6aa5d89aacf0
preethamgunda <69001955+preethamgunda@users.noreply.github.com>
2021-09-16 14:13:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+fetch('https://www.scorebat.com/video-api/v3/')
+    .then(response => response.text())
+    .then(data => console.log(data));
+    async function fetchText() {
+      let response = await fetch('https://www.scorebat.com/video-api/v3/');
+      console.log(response.status); // 200
+    console.log(response.statusText); // OK
+
+    if (response.status === 200) {
+        let data = await response.text();
+        // handle data
+    }
+}
+
+fetchText();
+async function getUsers() {
+  let url = 'users.json';
+  try {
+      let res = await fetch(url);
+      return await res.json();
+  } catch (error) {
+      console.log(error);
+  }
+}
