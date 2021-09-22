37b19d80e32a1c3091a88a03298b0ffc3bf293e6
preethamgunda <69001955+preethamgunda@users.noreply.github.com>
2021-09-22 12:01:44 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-fetch('https://www.scorebat.com/video-api/v3/')
-    .then(response => response.text())
-    .then(data => console.log(data));
-    async function fetchText() {
-      let response = await fetch('https://www.scorebat.com/video-api/v3/');
-      console.log(response.status); // 200
-    console.log(response.statusText); // OK
 
-    if (response.status === 200) {
-        let data = await response.text();
-        // handle data
-    }
-}
 
-fetchText();
-async function getUsers() {
-  let url = 'users.json';
-  try {
-      let res = await fetch(url);
-      return await res.json();
-  } catch (error) {
-      console.log(error);
-  }
-}
-
+fetch("https://api-football-v1.p.rapidapi.com/v3/timezone", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "SIGN-UP-FOR-KEY",
+		"x-rapidapi-host": "api-football-v1.p.rapidapi.com"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+  
+  
